8802f397427b2af889793eba07aed845027be592
Daniel Rocha Franca <112904598+Dan007p4@users.noreply.github.com>
2024-01-23 10:54:49 -0300

    Update Version11092023.py

--- Version11092023.py
+++ Version11092023.py
@@ -43,7 +43,14 @@ connection = mysql.connector.connect(
 
 
 c = connection.cursor()
-
+hide_st_style = """
+             <style>
+             #MainMenu {visibility: hidden;}
+             footer {visibility: hidden;}
+             header {visibility: hidden;}
+             </style>
+             """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 def get_manager():
     return stx.CookieManager()
